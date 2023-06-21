889ef821b1d505ae6f102117e2a0f408f28f31ae
dhhVo1 <vohuynhhaidang24032001@gmail.com>
2023-06-21 13:55:38 +0700

    v9

--- contracts/interfaces/IFEToken.sol
+++ contracts/interfaces/IFEToken.sol
@@ -4,14 +4,8 @@ pragma solidity ^0.8.9;
 interface IFEToken {
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    // event Approval(address indexed owner, address indexed spender, uint256 value);
 
     function mint(address to, uint256 amount) external;
     function burn(address to, uint256 amount) external;
-    // function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    // function transfer(address to, uint256 amount) external returns (bool);
-    // function allowance(address owner, address spender) external view returns (uint256);
-    // function approve(address spender, uint256 amount) external returns (bool);
-    // function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
